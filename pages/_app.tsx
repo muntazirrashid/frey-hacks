1069fb96af92922c4b6359498e3927793469771f
Muntazir Rashid <muntazirrashid@Muntazirs-MacBook-Pro.local>
2022-06-25 23:24:31 +0100

    Footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import NavBar from '../components/NavBar';
+import FixedBottomNavigation from '../components/Footer';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <NavBar />
       <Component {...pageProps} />
+      <FixedBottomNavigation />
     </>
   );
 }
